06544034348a3286c25acf0b8cdd29fc7b2c9c19
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2026-03-22 10:49:30 +0600

    Feat: Implement tenstack query function

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/sonner";
 import Transition from "@/components/shared/Transition";
+import QueryProviders from "@/providers/queryProvider";
 
 const dmSans = DM_Sans({ subsets: ['latin'], variable: '--font-dm-sans' });
 const playfair = Playfair_Display({ subsets: ['latin'], variable: '--font-playfair' });
@@ -24,9 +25,11 @@ export default function RootLayout({
       className={cn("h-full", "antialiased", dmSans.variable, playfair.variable)}
     >
       <body className="min-h-full flex flex-col">
-        <Transition>
-          {children}
-        </Transition>
+        <QueryProviders>
+          <Transition>
+            {children}
+          </Transition>
+        </QueryProviders>
         <Toaster />
       </body>
     </html>
